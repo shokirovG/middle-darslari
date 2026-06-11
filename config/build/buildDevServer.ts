2530188ac4598915ee600e7d6e61c5fd8d8894f7
Your Name <you@example.com>
2026-06-11 14:10:04 +0500

    open:isDevga boglandi

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -5,7 +5,7 @@ import type {Configuration as DevServerConfiguration} from "webpack-dev-server"
 export function buildDevServer(options:BuildOptions): DevServerConfiguration {
         return {
             port: options.port,
-            open: true,
+            open: options.isDev,
             historyApiFallback: true,
             hot: true,
         }
